c7ee4db847fd5dd3dcda4da818e22debef97cf75
Armando Flores <Armando.FloresIbarra@firstdata.com>
2018-03-28 16:31:45 -0400

    Trying to configurate a JTA Data Source

--- src/main/java/application/rest/LibertyRestEndpoint.java
+++ src/main/java/application/rest/LibertyRestEndpoint.java
@@ -1,13 +1,29 @@
 package application.rest;
 
+import model.Person;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Path("/")
 public class LibertyRestEndpoint {
 
+//    private EntityManager entityManager;
+//
+//    @PostConstruct
+//    private void setup() {
+//        entityManager = Persistence.createEntityManagerFactory("puPostgres").createEntityManager();
+//    }
+
     @GET
     public String hello() {
+        EntityManager entityManager = Persistence.createEntityManagerFactory("puPostgres").createEntityManager();
+        entityManager.persist(new Person("Armando"));
+
         return "Hello from the REST endpoint!";
     }
 }
---
+++ src/main/java/model/Person.java
@@ -0,0 +1,50 @@
+package model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "NAME", nullable = false, unique = true)
+    private String name;
+
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id) &&
+                Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
